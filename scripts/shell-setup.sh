f9998cf0e30fdd7c9cea3b33698148fd7223794f
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-03 05:24:52 +0000

    Fix and cleanup alias for cancelling last pipeline run

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -2,4 +2,4 @@
 
 export DEMO_HOME=$( cd "$(dirname "$0")/.." ; pwd -P )
 
-alias cpr="tkn pr cancel \$(tkn pr list -o name --limit 1 | cut -f 2 -d '/')"
+alias cpr='tkn pr cancel $(tkn pr list -o name --limit 1 | cut -f 2 -d "/")'
