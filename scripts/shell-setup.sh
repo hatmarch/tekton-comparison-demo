6a85d6460cb69ce2e19638bec3b5338c25ab3097
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-27 00:14:52 +0000

    Add convenience functions to shell setup script

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -3,3 +3,15 @@
 export DEMO_HOME=$( cd "$(dirname "$0")/.." ; pwd -P )
 
 alias cpr='tkn pr cancel $(tkn pr list -o name --limit 1 | cut -f 2 -d "/")'
+alias ctr='tkn tr cancel $(tkn tr list -o name --limit 1 | cut -f 2 -d "/")'
+
+# shorthand for creating a pipeline run file and watching the logs
+pr () {
+    FILE="$1"
+    oc create -f $FILE && tkn pr logs -L -f
+}
+
+tskr () {
+    FILE="$1"
+    oc create -f $FILE && tkn tr logs -L -f
+}
