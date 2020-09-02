b8cd93c6c0eb9e76a7b707103322a0cd76c0557b
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-09-02 08:31:19 +0000

    Add aws-up and aws-down convenience functions

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -15,3 +15,37 @@ tskr () {
     FILE="$1"
     oc create -f $FILE && tkn tr logs -L -f
 }
+
+aws-up() {
+    local CLUSTER_NAME=${1:-${CLUSTERNAME}}
+    if [[ -z "${CLUSTER_NAME}" ]]; then
+        echo "Must provide a cluster name either as parameter or in environment variable `CLUSTERNAME`"
+        return 1
+    fi
+
+    local AWS_REGION=${REGION}
+    if [[ -z "${AWS_REGION}" ]]; then
+        echo "Must provide a region by way of REGION environment variable"
+        return 1
+    fi
+
+    aws ec2 start-instances --instance-ids --region=${AWS_REGION} \
+        $(aws ec2 describe-instances --region ${AWS_REGION} --query 'Reservations[*].Instances[*].{Instance:InstanceId}' --output text --filters "Name=tag-key,Values=kubernetes.io/cluster/${CLUSTER_NAME}-*" "Name=instance-state-name,Values=stopped")
+}
+
+aws-down() {
+    local CLUSTER_NAME=${1:-${CLUSTERNAME}}
+    if [[ -z "$CLUSTER_NAME" ]]; then
+        echo "Must provide a cluster name either as parameter or in environment variable `CLUSTERNAME`"
+        return 1
+    fi
+
+    local AWS_REGION=${REGION}
+    if [[ -z "${AWS_REGION}" ]]; then
+        echo "Must provide a region by way of REGION environment variable"
+        return 1
+    fi
+
+    aws ec2 stop-instances --instance-ids --region ${AWS_REGION} \
+        $(aws ec2 describe-instances --region ${AWS_REGION} --query 'Reservations[*].Instances[*].{Instance:InstanceId}' --output text --filters "Name=tag-key,Values=kubernetes.io/cluster/${CLUSTER_NAME}-*" "Name=instance-state-name,Values=running") 
+}
