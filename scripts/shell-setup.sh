a4d26d349314269e7e0caa45fea17de39dceb887
Marc Hildenbrand <mwhildenbrand@gmail.com>
2020-09-06 13:41:46 +0000

    Add ability to specify project for pr function

--- scripts/shell-setup.sh
+++ scripts/shell-setup.sh
@@ -17,7 +17,9 @@ alias ctr='tkn tr cancel $(tkn tr list -o name --limit 1 | cut -f 2 -d "/")'
 # shorthand for creating a pipeline run file and watching the logs
 pr () {
     FILE="$1"
-    oc create -f $FILE && tkn pr logs -L -f
+    PRJ="${2:-petclinic-cicd}"
+    oc create -f $FILE -n $PRJ
+    tkn pr logs -L -f -n $PRJ
 }
 
 tskr () {
